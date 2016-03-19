4a296119419ac912a1d4a3ec83f48c313a729180
iamjackswastedlife <iamjacksleastwastedlife@gmail.com>
2016-03-19 12:44:55 +0200

    probaby final

---
+++ public/js/slider.js
@@ -0,0 +1,10 @@
+$("#slider").vegas({
+    timer: false,
+    transition: 'random',
+    slides: [
+        { src: "img/content/slide-1.jpg" },
+        { src: "img/content/slide-2.jpg" },
+        { src: "img/content/slide-3.jpg" },
+        { src: "img/content/slide-4.jpg" }
+    ]
+});
